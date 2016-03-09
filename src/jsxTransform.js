11e6f28230f5053a909823e287bef838c1da13bf
geowarin <geowarin@gmail.com>
2016-03-09 21:00:01 +0100

    Improve error handling

--- src/jsxTransform.js
+++ src/jsxTransform.js
@@ -37,36 +37,27 @@ function transform(filename, source, options) {
 
 function install(options) {
     if (installed) {
-        return
+        return;
     }
 
-    var fs = require('fs');
-    var Module = require('module');
-    var _require = Module.prototype.require;
-
     options = options || {};
+    if (!options.hasOwnProperty('sourceMapInline')) {
+        options.sourceMapInline = true;
+    }
 
-    Module._extensions[options.extension || '.jsx'] = function (module, filename) {
-        if (!options.hasOwnProperty('sourceMapInline')) {
-            options.sourceMapInline = true
-        }
-
-        var content = fs.readFileSync(filename, 'utf8');
+    require.extensions[options.extension || '.jsx'] = function loadJsx(module, filename) {
+        var content = require('fs').readFileSync(filename, 'utf8');
         try {
             var instrumented = transform(filename, content, options);
-            module._compile(instrumented, filename)
+            module._compile(instrumented, filename);
         } catch (e) {
-            console.error("Error compiling " + filename, e);
-            console.error(e.stack);
+            if (e.originalError) {
+                throw e.originalError;
+            }
+            e.message = 'Error compiling ' + filename + ': ' + e.message;
+            e.originalError = e;
+            throw e;
         }
     };
-
-    Module.prototype.require = function (filename) {
-        if ('!' === filename.slice(-1)) {
-            filename = filename.slice(0, -1)
-        }
-        return _require.call(this, filename)
-    };
-
     installed = true
 }
