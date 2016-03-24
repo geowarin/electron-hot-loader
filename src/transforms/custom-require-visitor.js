8e68c94f99410dbb125a1012c020fd86bbc61796
geowarin <geowarin@gmail.com>
2016-03-24 23:26:03 +0100

    Fix windows paths

--- src/transforms/custom-require-visitor.js
+++ src/transforms/custom-require-visitor.js
@@ -1,6 +1,6 @@
 var utils = require('jstransform/src/utils');
 var globalUtils = require('./utils');
-var requireRegister = require.resolve('../proxies');
+var requireRegister = require.resolve('../proxies').replace(/\\/g, '/');
 
 function requireVisitor (traverse, node, path, state) {
   if (!state.g.alreadyAddedElectronHotRequire) {
